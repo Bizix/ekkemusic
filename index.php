8d8291959153a7b42bc0423a1b9b698c872add54
Sean Sernasie <Sean@scarYviruS.local>
2019-09-03 16:39:36 +0900

    header added

--- index.php
+++ index.php
@@ -10,8 +10,28 @@
 </head>
 
 <body>
-
-    <h1>David's test</h1>
+    <header id="header">
+        <div class="socialmedia">
+            <div id="facebook">Facebook Icon</div>
+            <div id="twitter">Twitter Icon</div>
+            <div id="youtube">YouTube Icon</div>
+            <div id="instagram">Instagram Icon</div>
+            <div id="apple">Apple Icon</div>
+            <div id="spotify">Spotify Icon</div>
+        </div>
+        <div class="logo"><img src="ekkelogo.svg" alt=""></div>
+        <div id="nav">
+            <div id="home">Home</div>
+            <div id="music">music</div>
+            <!-- https://songwhip.com/artist/ekke?fbclid=IwAR2e3HppUviP0UTOST9uu17WN530BrsC7pzWhqbJ1mZ7lP-edjtQZxpPIuQ -->
+            <div id="shows">Shows</div>
+            <div id="about">About</div>
+            <div id="contact">Contact</div>
+        </div>
+    </header>
+    <section></section>
+    <section></section>
+    <section></section>
     <script src="scripts/ekke.js"></script>
 </body>
 
