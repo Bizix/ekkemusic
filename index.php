c45bed274cabbb32571cd0d7c1b720c32c038362
Sean Sernasie <Sean@scarYviruS.local>
2019-09-04 14:39:15 +0900

    changed video

--- index.php
+++ index.php
@@ -2,11 +2,11 @@
 <html lang="en">
 
 <head>
-    <link rel="shortcut icon" href="images/ekkelogo.ico" type="image/x-icon"/>
+    <link rel="shortcut icon" href="images/ekkelogo.ico" type="image/x-icon" />
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link href="https://fonts.googleapis.com/css?family=Montserrat&display=swap" rel="stylesheet"> 
+    <link href="https://fonts.googleapis.com/css?family=Montserrat&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="styles/main.css">
     <script src="https://kit.fontawesome.com/f76a3c0c83.js"></script>
     <title>EkkeMusic</title>
@@ -19,13 +19,14 @@
                 <div class="socialMedia">
                     <a href="https://www.facebook.com/itsekke" target="_blank" id="facebook"><i
                             class="fab fa-facebook-f"></i></a>
-                    <a href="https://twitter.com/itsekke" target="_blank" id="twitter"><i class="fab fa-twitter"></i></a>
+                    <a href="https://twitter.com/itsekke" target="_blank" id="twitter"><i
+                            class="fab fa-twitter"></i></a>
                     <a href="https://www.youtube.com/channel/UC1LupZh5xH3EATWUtHr07RA" target="_blank" id="youtube"><i
                             class="fab fa-youtube"></i></a>
                     <a href="https://www.instagram.com/itsekke/" target="_blank" id="instagram"><i
                             class="fab fa-instagram"></i></a>
-                    <a href="https://music.apple.com/ca/artist/ekke/1223175655?app=music&ign-mpt=uo%3D4" target="_blank" id="apple"><i
-                            class="fab fa-apple"></i></a>
+                    <a href="https://music.apple.com/ca/artist/ekke/1223175655?app=music&ign-mpt=uo%3D4" target="_blank"
+                        id="apple"><i class="fab fa-apple"></i></a>
                     <a href="https://open.spotify.com/artist/5PLFMpk4XHu8rdpKu4dIta" target="_blank" id="spotify"><i
                             class="fab fa-spotify"></i></a>
                 </div>
@@ -33,7 +34,9 @@
                 <nav class="navbar_container">
                     <ul class="links">
                         <li class="link"><a href="#">Home</a></li>
-                        <li class="link"><a href="https://songwhip.com/artist/ekke?fbclid=IwAR2e3HppUviP0UTOST9uu17WN530BrsC7pzWhqbJ1mZ7lP-edjtQZxpPIuQ" target="_blank">Music</a></li>
+                        <li class="link"><a
+                                href="https://songwhip.com/artist/ekke?fbclid=IwAR2e3HppUviP0UTOST9uu17WN530BrsC7pzWhqbJ1mZ7lP-edjtQZxpPIuQ"
+                                target="_blank">Music</a></li>
                         <li class="link"><a href="#">Shows</a></li>
                         <li class="link"><a href="#">About</a></li>
                         <li class="link"><a href="#">Contact</a></li>
@@ -46,12 +49,14 @@
                 <img src="images/Shoots/1.jpg" alt="">
             </div>
             <div class="hero_video">
-                <iframe width="720" height="420" src="https://www.youtube.com/embed/NQi8YZGFxGo" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                <iframe width="720" height="420" src="https://www.youtube.com/embed/axnt-TT6N4I" frameborder="0"
+                    allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+                    allowfullscreen></iframe>
             </div>
         </div>
         <div class="scroll_icon">
             <div class="wheel_animation">
-            </div> 
+            </div>
         </div>
     </section>
     <script src="scripts/ekke.js"></script>
