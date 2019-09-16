1ec2949a5697ad8dff26b57558af7a4da1254702
drodrigues21 <ddvilar21@hotmail.com>
2019-09-16 17:07:11 +0900

    added footer and small tweaks

--- index.php
+++ index.php
@@ -42,12 +42,9 @@
             </div>
         </header>
         <div class="hero_container">
-            <div class="hero_img">
+            <!-- <div class="hero_img">
                 <img src="images/Shoots/image_from_ios.jpg" alt="">
-            </div>
-            <div class="hero_video">
-                <iframe width="720" height="420" src="https://www.youtube.com/embed/NQi8YZGFxGo" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-            </div>
+            </div> -->
         </div>
         <div class="scroll_icon">
             <div class="wheel_animation">
@@ -55,18 +52,100 @@
         </div>
     </section>
     <section id="shows_container">
-
+        <div class="shows_content">
+            <h2>Upcoming Shows</h2>
+            <div class="shows">
+                <div class="show_header">
+                    <p>Date</p>
+                    <p>Location</p>
+                    <p>Sales</p>
+                </div>
+                <div class="show">
+                    <div class="show_date">
+                        <p class="top_date">Fri</p>
+                        <p class="middle_date">Nov 7</p>
+                        <p class="bottom_date">2019</p>
+                    </div>
+                    <div class="show_location"><p>Auckland, NZ</p></div>
+                    <div class="show_ticket"><button>Ticket</button></div>
+                </div>
+                <div class="show">
+                    <div class="show_date">
+                        <p class="top_date">Fri</p>
+                        <p class="middle_date">Nov 15</p>
+                        <p class="bottom_date">2019</p>
+                    </div>
+                    <div class="show_location"><p>Sydney, AU</p></div>
+                    <div class="show_ticket"><button>Ticket</button></div>
+                </div>
+                <div class="show">
+                    <div class="show_date">
+                        <p class="top_date">Thu</p>
+                        <p class="middle_date">Dec 5</p>
+                        <p class="bottom_date">2019</p>
+                    </div>
+                    <div class="show_location"><p>Tokyo, JP</p></div>
+                    <div class="show_ticket"><button>Ticket</button></div>
+                </div>
+                <div class="show">
+                    <div class="show_date">
+                        <p class="top_date">Sun</p>
+                        <p class="middle_date">Dec 8</p>
+                        <p class="bottom_date">2019</p>
+                    </div>
+                    <div class="show_location"><p>Seoul, KR</p></div>
+                    <div class="show_ticket"><button>Ticket</button></div>
+                </div>
+            </div>
+        </div>
     </section>
     <section id="about_container">
         <div class="about">
-           <h2>About Me</h2>
-           <p>Equal parts ignorant and clever, the music of rapper/producer/songwriter Ekke covers a range of topics over an eccentric variety of genre-bending beats, some of which are self-produced.</p>
-           <p>Born in Accra, Ghana, Ekke moved with his family to New Jersey, where he lived from ages 4-7. Returning to Ghana by age 8, he grew up changing schools and moving houses all around Accra. He came to Canada in 2010 to attend the University of British Columbia, graduating with a Bachelor of Commerce from the UBC Sauder School of Business in 2015.</p>
-           <p>Ekke has been exposed to a variety of genres throughout his upbringing, from Motown Funk and Jazz to Alternative Rock and of course, hip-hop. Influenced by rappers such as Eminem, Kanye West, The Game, Lil’ Wayne, Dr. Dre, Lupe Fiasco & Jay-Z, Ekke fell in love with hip-hop primarily for the lyricism and story telling. Both of these are major focal points in his own music.</p>
-           <p>Ekke makes use of clever word play, exploration of dark & exaggerated themes, hyperbole and complex rhyme schemes to paint pictures and tell stories. Initially rapping under the moniker ‘Palendroam’, Ekke honed his craft writing original lyrics over popular beats before transitioning into production and creation of entirely original material.</p>
-        
+        <h2>About Me</h2>
+        <p>Equal parts ignorant and clever, the music of rapper/producer/songwriter Ekke covers a range of topics over an eccentric variety of genre-bending beats, some of which are self-produced.</p>
+        <p>Born in Accra, Ghana, Ekke moved with his family to New Jersey, where he lived from ages 4-7. Returning to Ghana by age 8, he grew up changing schools and moving houses all around Accra. He came to Canada in 2010 to attend the University of British Columbia, graduating with a Bachelor of Commerce from the UBC Sauder School of Business in 2015.</p>
+        <p>Ekke has been exposed to a variety of genres throughout his upbringing, from Motown Funk and Jazz to Alternative Rock and of course, hip-hop. Influenced by rappers such as Eminem, Kanye West, The Game, Lil’ Wayne, Dr. Dre, Lupe Fiasco & Jay-Z, Ekke fell in love with hip-hop primarily for the lyricism and story telling. Both of these are major focal points in his own music.</p>
+        <p>Ekke makes use of clever word play, exploration of dark & exaggerated themes, hyperbole and complex rhyme schemes to paint pictures and tell stories. Initially rapping under the moniker ‘Palendroam’, Ekke honed his craft writing original lyrics over popular beats before transitioning into production and creation of entirely original material.</p>
         </div>
+        <footer id="footer_container">
+            <div class="footer">
+                <div class="socialMedia social_footer">
+                    <a href="https://www.facebook.com/itsekke" target="_blank" id="facebook"><i
+                            class="fab fa-facebook-f"></i></a>
+                    <a href="https://twitter.com/itsekke" target="_blank" id="twitter"><i class="fab fa-twitter"></i></a>
+                    <a href="https://www.youtube.com/channel/UC1LupZh5xH3EATWUtHr07RA" target="_blank" id="youtube"><i
+                            class="fab fa-youtube"></i></a>
+                    <a href="https://www.instagram.com/itsekke/" target="_blank" id="instagram"><i
+                            class="fab fa-instagram"></i></a>
+                    <a href="https://music.apple.com/ca/artist/ekke/1223175655?app=music&ign-mpt=uo%3D4" target="_blank" id="apple"><i
+                            class="fab fa-apple"></i></a>
+                    <a href="https://open.spotify.com/artist/5PLFMpk4XHu8rdpKu4dIta" target="_blank" id="spotify"><i
+                            class="fab fa-spotify"></i></a>
+                </div>
+                <div class="contact_me"><p><span>Contact:<span> ekke@gmail.com</p></div>
+                <div class="copyright"><p>&copy; Created by Sean Sernasie and David Vilar</p></div>
+            </div>
+    </footer>
     </section>
+    <!-- <footer id="footer_container">
+        <div class="footer">
+            <div class="socialMedia social_footer">
+                <a href="https://www.facebook.com/itsekke" target="_blank" id="facebook"><i
+                        class="fab fa-facebook-f"></i></a>
+                <a href="https://twitter.com/itsekke" target="_blank" id="twitter"><i class="fab fa-twitter"></i></a>
+                <a href="https://www.youtube.com/channel/UC1LupZh5xH3EATWUtHr07RA" target="_blank" id="youtube"><i
+                        class="fab fa-youtube"></i></a>
+                <a href="https://www.instagram.com/itsekke/" target="_blank" id="instagram"><i
+                        class="fab fa-instagram"></i></a>
+                <a href="https://music.apple.com/ca/artist/ekke/1223175655?app=music&ign-mpt=uo%3D4" target="_blank" id="apple"><i
+                        class="fab fa-apple"></i></a>
+                <a href="https://open.spotify.com/artist/5PLFMpk4XHu8rdpKu4dIta" target="_blank" id="spotify"><i
+                        class="fab fa-spotify"></i></a>
+            </div>
+            <div class="contact_me"><p><span>Contact:<span> ekke@gmail.com</p></div>
+            <div class="copyright"><p>&copy; Created by Sean Sernasie and David Vilar</p></div>
+        </div>
+    </footer> -->
     <script src="scripts/ekke.js"></script>
 </body>
 
