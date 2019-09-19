408dcf47d2a2d68879017b79f7149c7a5459a594
Sean Sernasie <Sean@scarYviruS.local>
2019-09-19 15:52:14 +0900

    Youtube WORKING

--- scripts/ekke.js
+++ scripts/ekke.js
@@ -2,7 +2,7 @@ var getYoutube = function () {
     var yxhr = new XMLHttpRequest();
     var key = "AIzaSyAtO185zLEYwvnXo_fA4_MFJhwkTPBfaDo";
     var youtubeID;
-
+    let video_id;
     // PROBLEM 1
     // reference site : https://developers.google.com/youtube/v3/guides/implementation/playlists
 
@@ -14,16 +14,18 @@ var getYoutube = function () {
     // '&key=' + key;
 
     // Part 2 
-    var url = 'https://developers.google.com/apis-explorer/#p/youtube/v3/youtube.playlists.list? ' +
-        'part=snippet,contentDetails' +
-        '&channelId=UC1LupZh5xH3EATWUtHr07RA';
+    // var url = "https://www.googleapis.com/youtube/v3/playlists?part=snippet%2CcontentDetails&channelId=UC1LupZh5xH3EATWUtHr07RA&key=" + key;
+    var playlistID = 'PLapZNv8wV6KaeTnDpx0ArdbfuWPYwbA0r';
+    var url = "https://www.googleapis.com/youtube/v3/playlistItems?part=contentDetails&maxResults=1&playlistId=" + playlistID + "&key=" + key;
 
-    // Goal is to load most recently uploaded video in the playlist "videos"
 
     yxhr.open('GET', url, false);
     yxhr.onreadystatechange = function () {
         if (yxhr.readyState === XMLHttpRequest.DONE && yxhr.status === 200) {
+            console.log(yxhr.responseText);
             let objectYT = JSON.parse(yxhr.responseText);
+            // console.log(objectYT);
+            youtubeID = objectYT.items[0].contentDetails.videoId;
             var youtube_container = document.querySelector('#youtube_container');
             youtube_container.innerHTML = ' <iframe id="ytplayer" type="text/html" width="896" height="504" src="https://www.youtube.com/embed/' + youtubeID + '"frameborder="0"></iframe>';
             // youtube_container.innerHTML = most recent upload in playlist "videos" (not first in playlist)
@@ -46,28 +48,32 @@ var getYoutube = function () {
 
 var getInsta = function () {
     var xhr = new XMLHttpRequest();
+    var clientID = "8d0fe584f3d141af9193eb075656123b";
     var url = "https://api.instagram.com/oembed?url=https://www.instagram.com/itsekke/";
+    url = "https://api.instagram.com/oauth/authorize/?client_id=" + clientID + "&redirect_uri=http://localhost:8888/Sites/Ekke/index.php&response_type=token";
+
     xhr.open('GET', url, true);
     xhr.onreadystatechange = function () {
         if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
-            let objectIG = JSON.parse(xhr.responseText);
-            for (var i = 0; i < data.data.length; i++) {
-                var container = document.getElementById('insta-feed');
-                var imgURL = data.data[i].images.standard_resolution.url;
-                var div = document.createElement('div');
-                div.setAttribute('class', 'instapic');
-                var insta_container = document.querySelector('#insta_container');
-                insta_container.appendChild(div);
-                var img = document.createElement('img');
-                img.setAttribute('src', imgURL)
-                div.appendChild(img);
-            }
+            console.log(xhr.responseText);
+            // let objectIG = JSON.parse(xhr.responseText);
+            // for (var i = 0; i < data.data.length; i++) {
+            //     var container = document.getElementById('insta-feed');
+            //     var imgURL = data.data[i].images.standard_resolution.url;
+            //     var div = document.createElement('div');
+            //     div.setAttribute('class', 'instapic');
+            //     var insta_container = document.querySelector('#insta_container');
+            //     insta_container.appendChild(div);
+            //     var img = document.createElement('img');
+            //     img.setAttribute('src', imgURL)
+            //     div.appendChild(img);
+            //}
 
         }
     }
     xhr.send();
 }
 
-getYoutube;
-getInsta;
+getYoutube();
+// getInsta();
 
